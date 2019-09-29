e5e911e16385cf11c22bb09026c0e9b719fd8c44
elizabeth <ellizzabbetth@gmail.com>
2019-09-29 11:54:21 -0400

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // When app starts up check to see if we have a database.
+            // if not, then create db.
+            
+            // creates database in API project
+            var host = CreateWebHostBuilder(args).Build();
+            // want reference to dataContext while executing Main method
+            // 
+            using (var scope = host.Services.CreateScope()) 
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+
+                }
+                catch (System.Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                    
+                }
+                host.Run();
+            }
+            // CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Domain/Value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        // auto implemented properties
+        // type prop then tab
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
